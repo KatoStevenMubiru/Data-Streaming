e4cdaea4de6d53e94c151c7cef6a39b94d96bfde
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-01-24 14:27:43 +0300

    Create submit-atm-visits.sh

---
+++ submit-atm-visits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the path to the spark-submit executable
+# Replace this with the actual path if necessary
+SPARK_SUBMIT="/path/to/spark/bin/spark-submit"
+
+# Define the path to your python script
+PYTHON_SCRIPT="/home/workspace/atm-visits.py"
+
+# Submit the application to the Spark cluster
+$SPARK_SUBMIT --master [Spark URI] $PYTHON_SCRIPT
